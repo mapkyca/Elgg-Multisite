cb78db9a1efa39b4aa5ffffa3cd3229c7d34602d
mapkyca <marcus@dushka.co.uk>
2010-04-04 17:43:36 +0000

    Domains display
    git-svn-id: https://code.marcus-povey.co.uk/unofficialelgg/products/elggmultisite@63 99efaf4b-db9e-4e6e-8f48-b80637b551b9

--- elggmulti/multi/pages/forms/domains.php
+++ elggmulti/multi/pages/forms/domains.php
@@ -1,14 +1,37 @@
 <?php
-
+	$forms = MultisiteDomain::getDomainTypes();
 ?>
 <div class="domains">
-
-
-
+<?php
+	$domains = elggmulti_getdata("SELECT * from domains", '__elggmulti_db_row');
+	
+	if ($domains)
+	{
+		foreach ($domains as $domain)
+		{
+			$label = $forms[get_class($domain)];
+			$url = $domain->getDomain();
+		?>
+		<div class="domain">
+			<a href="#" onclick="showhide('add_<?php echo $domain; ?>');"><?php echo $label; ?> <?php echo $url; ?></a>
+			<div id="disp_<?php echo $domain; ?>" style="display: none;">
+				<div class="domain_display">
+				<?php include(dirname(__FILE__)."/../objects/{$d}.php"); ?>
+				</div>
+			</div>
+			<div class="domain_strap">
+				Edit :: Delete
+			</div>
+		</div>	
+		<?php
+			
+		}
+	}
+?>	
+</div>
+<div class="domain_forms">
 	<?php
 
-		$forms = MultisiteDomain::getDomainTypes();
-		
 		if ($forms)
 		{
 			foreach ($forms as $domain => $label) {	
