c5555d8f57799cb2dac936254925ece6e47dbf27
Marcus Povey <marcus@marcus-povey.co.uk>
2012-07-03 14:20:27 +0100

    Removing whitespace which was causing embed problems on some hosts.

--- elggmulti/docroot/mod/pluginmanager/views/default/admin/plugins.php
+++ elggmulti/docroot/mod/pluginmanager/views/default/admin/plugins.php
@@ -200,38 +200,3 @@ if ($show_category == 'all' && $sort == 'priority') {
 echo elgg_view_entity_list($plugin_list, $options);
 ?>
 </div>
-<?php
-
-/*	global $CONFIG;
-	
-	$ts = time();
-	$token = generate_action_token($ts);
-	
-	// Description of what's going on
-	$buttons = " <a class='enableallplugins' href=\"{$CONFIG->url}action/admin/plugins/enableall?__elgg_token=$token&__elgg_ts=$ts\">".elgg_echo('enableall')."</a>  <a class='disableallplugins' href=\"{$CONFIG->url}action/admin/plugins/disableall?__elgg_token=$token&__elgg_ts=$ts\">".elgg_echo('disableall')."</a> ";
-	echo "<div class=\"contentWrapper\"><span class=\"contentIntro\">" . $buttons . elgg_view('output/longtext', array('value' => elgg_echo("admin:plugins:description"))) . "<div class='clearfloat'></div></span></div>";
-
-	
-	$limit = get_input('limit', 10);
-	$offset = get_input('offset', 0);
-	
-	// Get the installed plugins
-	$installed_plugins = $vars['installed_plugins'];
-	$count = count($installed_plugins);
-	
-	$plugin_list = elgg_get_plugins('any');
-	print_r($installed_plugins);
-	$max = 0;
-	foreach($plugin_list as $key => $foo) {
-		if ($key > $max) $max = $key;
-	}
-
-	// Display list of plugins
-	$n = 0;
-	foreach ($installed_plugins as $plugin => $data) 
-	{
-		echo elgg_view("admin/plugins_opt/plugin", array('plugin' => $plugin, 'details' => $data, 'maxorder' => $max, 'order' => array_search($plugin, $plugin_list)));
-	
-		$n++;
-	}
-?>
