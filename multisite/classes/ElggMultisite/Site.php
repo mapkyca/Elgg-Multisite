b8152aff900181f84d8f06e86dac0a0d43172fc8
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-07 15:38:59 +0000

    Moving multisite plugin tools to site

--- multisite/classes/ElggMultisite/Site.php
+++ multisite/classes/ElggMultisite/Site.php
@@ -26,6 +26,83 @@ namespace ElggMultisite {
 		throw new \Exception("There was a problem connecting to the multisite directory.");
 	    }
 	}
+	
+	
+	/**
+	 * Return a list of all installed plugins.
+	 *
+	 */
+	public function getInstalledPlugins()
+	{
+	    $plugins = array();
+
+	    $path = dirname(dirname(dirname(dirname(__FILE__)))) . '/elgg/mod/';
+
+	    if ($handle = opendir($path)) {
+
+		    while ($mod = readdir($handle)) {
+
+			    if (!in_array($mod,array('.','..','.svn','CVS', '.git')) && is_dir($path . "/" . $mod)) {
+				    if ($mod!='pluginmanager') // hide plugin manager
+					    $plugins[] = $mod;
+			    }
+
+		    }
+	    }
+
+	    sort($plugins);
+
+	    return $plugins;
+	}
+	
+	/**
+	 * Get plugins which have been activated for a given domain.
+	 *
+	 * @param int $domain_id
+	 * @return array|false
+	 */
+	function getActivatedPlugins($domain_id = null)
+	{
+	    if (!$domain_id)
+	    {
+		$router = new Router();
+		$result = $router->route();
+
+		$domain_id = $result->getID();
+	    }
+
+	    $domain_id = (int)$domain_id;
+
+	    $result = DB::execute("SELECT * from domains_activated_plugins where domain_id=:domain_id", [':domain_id' => $domain_id]);
+	    $resultarray = array();
+	    foreach ($result as $r)
+		$resultarray[] = $r->plugin;
+
+	    return $resultarray;
+	}
+	
+	/**
+	 * Activate or deactivate a plugin. 
+	 *
+	 * @param unknown_type $domain_id
+	 * @param unknown_type $plugin
+	 * @param unknown_type $activate
+	 */
+	function setActivatedPlugins($domain_id, $plugin, $activate = true)
+	{
+	    if ($activate)
+	    {
+		DB::insert("INSERT into domains_activated_plugins (domain_id, plugin) VALUES (:domain_id, :plugin)", [':domain_id' => $domain_id, ':plugin' => $plugin]);
+	    }
+	    else
+	    {
+		DB::delete("DELETE FROM domains_activated_plugins where domain_id=:domain_id and plugin=:plugin", [':domain_id' => $domain_id, ':plugin' => $plugin]);
+
+		$domain = Domain::getByID($domain_id);
+		if ($domain)
+		    $domain->disable_plugin($plugin);
+	    }
+	}
 
 	/**
 	 * 
