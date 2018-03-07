7b68181f6e5d3e439b57474b9449c924e7695b39
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-07 11:25:20 +0000

    Users page (removing new user for simplicity)

--- multisite/templates/default/pages/users.tpl.php
+++ multisite/templates/default/pages/users.tpl.php
@@ -1,1 +1,55 @@
-users
+<?php
+$users = \ElggMultisite\User::getUsers();
+
+?>
+<div class="users card">
+    <div class="card-header">Users</div>
+    <div class="card-body">
+    <ul class="list-group">
+    <?php
+    foreach ($users as $user) {
+	?>
+    
+    <li class="list-group-item">
+		<a href="#disp_<?php echo $user->id; ?>" data-toggle="collapse"><b><?php echo $user->username; ?></b></a>
+		
+		
+		<div id="disp_<?php echo $user->id; ?>" class="collapse">
+			<div class="user_display">
+				
+				<form action="/session/setpassword" method="POST">
+			
+					<div class="form-group">
+						<label>Password: <input class="password input-password form-control" type="password" name="password" value="" /> </label>
+					</div>
+					<div class="form-group">
+						<label>Password again: <input class="password input-password form-control" type="password" name="password2" value="" /> </label>
+					</div>
+					<input type="hidden" name="username" value="<?php echo $user->username; ?>" />
+					<input class="submit" type="submit" name="Save" value="Save">
+				</form>
+				
+			
+			</div>
+		</div>
+		
+		
+		
+		<div class="user_strap">
+			<?php if ((count($users)>1) && ($_SESSION['em_user']->username!=$user->username)) {?><a href="session/deleteuser?user_id=<?php echo $user->id; ?>">Delete</a><?php } ?>
+		</div>
+    </li>
+    
+    
+    <?php } ?>  
+    </ul>
+    </div>
+</div>
+<!--
+<br />
+<div class="users_forms card">
+    <div class="card-header">Add a new user...</div>
+	<div class="card-body">
+		<?= $this->draw('forms/register'); ?>
+	</div>
+</div>-->
