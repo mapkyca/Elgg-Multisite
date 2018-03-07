efcdea9798eea8211e437bd63166b816883fd06d
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-07 15:41:46 +0000

    Add domain

---
+++ multisite/classes/ElggMultisite/Pages/Domains/Add.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ElggMultisite\Pages\Domains {
+    
+    class Add extends \ElggMultisite\Page {
+	
+	public function get() {
+	    $this->gatekeeper();
+	    
+	}
+
+	public function post() {
+	    $this->gatekeeper();
+	    
+	    try {
+		\ElggMultisite\Domain::addDomain(
+			\ElggMultisite\Input::getInput('domain'), 
+			\ElggMultisite\Input::getInput('dbsettings'), 
+			\ElggMultisite\Input::getInput('available_plugins'), 
+			\ElggMultisite\Input::getInput('class')
+		);
+	    } catch (\Exception $e) {
+		\ElggMultisite\Messages::addMessage($e->getMessage());
+		throw $e;
+	    }
+	    
+	    $this->forward('/domains/');
+	}
+
+    }
+}
