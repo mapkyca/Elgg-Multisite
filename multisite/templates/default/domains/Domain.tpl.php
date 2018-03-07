cb116c46b1995051e57685ee607c1c7463aa2b5f
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-07 15:40:18 +0000

    Handle add/edit

--- multisite/templates/default/domains/Domain.tpl.php
+++ multisite/templates/default/domains/Domain.tpl.php
@@ -1,9 +1,12 @@
 <?php
 
+$form = '/domains/add';
 $domain = $vars['domain'];
+if (!empty($domain))
+    $form = '/domains/edit';
 ?>
 <div class="multisitedomain_edit">
-	<form action="/domains/add" method="post">
+	<form action="<?= $form; ?>" method="post">
 		
 		<div class="form-group">
 		    <label>Domain name: <input type="text" class="domain input-text form-control" name="domain" value="<?= (!empty($domain)) ? $domain->getDomain() : ''; ?>" placeholder="yoursite.multi" required /></label>
