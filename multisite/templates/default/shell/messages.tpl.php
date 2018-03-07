522ba3d830231ade2aa0ac8a0ad2c95c88621616
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-07 14:16:32 +0000

    Display messages

--- multisite/templates/default/shell/messages.tpl.php
+++ multisite/templates/default/shell/messages.tpl.php
@@ -1,11 +1,19 @@
 <div id="page-messages">
     <?php
+    $messages = ElggMultisite\Messages::getMessages();
+    if (!empty($messages)) {
+	foreach ($messages as $message) {
+	    ?>
+	    <div class="alert alert-primary" role="alert">
+		
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		    <span aria-hidden="true">&times;</span>
+		</button>
+		<?= $message ?>
+	    </div>
 
-        if (!empty($vars['messages'])) {
-            foreach ($vars['messages'] as $message) {
-                
-            }
-        }
-
+	    <?php
+	}
+    }
     ?>
 </div>
