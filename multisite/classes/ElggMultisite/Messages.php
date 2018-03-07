b1e0763d565a053c105999b7f417f7bdba15bc0c
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-07 14:16:22 +0000

    Messages handler

--- multisite/classes/ElggMultisite/Messages.php
+++ multisite/classes/ElggMultisite/Messages.php
@@ -7,14 +7,24 @@ namespace ElggMultisite {
 	
 	public static function addMessage($message) {
 	    
+	    if (empty($_SESSION['messages']))
+		$_SESSION['messages'] = [];
+	    
+	    if (!in_array($message, $_SESSION['messages']))
+		$_SESSION['messages'][] = $message;
 	}
 	
 	public static function getMessages() {
 	    
 	    
 	    // Get messages
+	    $messages = $_SESSION['messages'];
 	    
 	    // Clear messages
+	    unset($_SESSION['messages']);
+	    $_SESSION['messages'] = [];
+	    
+	    return $messages;
 	}
     }
 }
