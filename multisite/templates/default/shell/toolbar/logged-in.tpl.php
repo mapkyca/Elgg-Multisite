b9cc85abdac679072504c2abbce8d5dee1d61fe8
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-07 21:01:07 +0000

    Sorting out navbar

--- multisite/templates/default/shell/toolbar/logged-in.tpl.php
+++ multisite/templates/default/shell/toolbar/logged-in.tpl.php
@@ -1,4 +1,4 @@
 
-        <li class="nav-item"><a class="nav-link" href="domains">Domains</a></li>
-        <li class="nav-item"><a class="nav-link" href="users">Multisite Domain Admins</a></li>
-        <li class="nav-item"><a class="nav-link" href="session/logout">Log out</a></li>
+        <li class="nav-item"><a class="nav-link" href="/domains">Domains</a></li>
+        <li class="nav-item"><a class="nav-link" href="/users">Users</a></li>
+        <li class="nav-item"><a class="nav-link" href="/session/logout">Log out</a></li>
