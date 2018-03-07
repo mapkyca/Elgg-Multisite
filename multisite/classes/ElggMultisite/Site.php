8044da2e89888cd3b86b8fa87100569e9eae698b
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-07 14:16:50 +0000

    Check install on boot

--- multisite/classes/ElggMultisite/Site.php
+++ multisite/classes/ElggMultisite/Site.php
@@ -1,27 +1,37 @@
 <?php
 
 namespace ElggMultisite {
-    
+
     class Site {
-	
+
 	private static $site;
-	
+
+	public function getDataDir() {
+	    return dirname(dirname(dirname(dirname(__FILE__)))) . "/data/";
+	}
+
 	public function getWWWRoot() {
 	    return '/'; // TODO: Do it better
 	}
-	
-	
+
+	public function checkInstall() {
+
+	    if (!is_writable($this->getDataDir()))
+		throw new \Exception ("It doesn't appear that your data directory (".$this->getDataDir ().") is writable.");
+	}
+
 	/**
 	 * 
 	 * @return \ElggMultisite\Site
 	 */
 	public static function site() {
-	    
+
 	    if (empty(self::$site))
 		self::$site = new Site();
-	    
+
 	    return self::$site;
 	}
-	
+
     }
+
 }
