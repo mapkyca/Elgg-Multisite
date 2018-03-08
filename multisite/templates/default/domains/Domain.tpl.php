bded3210cb5f49ecc2470f6756c786e39abc94c5
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-08 11:38:06 +0000

    Adding ID of domain to edit

--- multisite/templates/default/domains/Domain.tpl.php
+++ multisite/templates/default/domains/Domain.tpl.php
@@ -8,14 +8,21 @@ if (!empty($domain))
 <div class="multisitedomain_edit">
 	<form action="<?= $form; ?>" method="post">
 		
-		<div class="form-group">
-		    <label>Domain name: <input type="text" class="domain input-text form-control" name="domain" value="<?= (!empty($domain)) ? $domain->getDomain() : ''; ?>" placeholder="yoursite.multi" required /></label>
-		</div>
-	    
-		<?= $this->draw('forms/domains/dbsettings'); ?>
-		<?= $this->draw('forms/domains/plugins'); ?>
-	    
-		<input type="hidden" name="class" value="ElggMultisite\Domain" />
-		<input type="submit" name="Submit" value="Submit" />
+	    <?php 
+	    if (!empty($domain)) {
+		?>
+	    <input type="hidden" name="id" value="<?= $domain->getID(); ?>" />
+	<?php
+	    }
+	    ?>
+	    <div class="form-group">
+		<label>Domain name: <input type="text" class="domain input-text form-control" name="domain" value="<?= (!empty($domain)) ? $domain->getDomain() : ''; ?>" placeholder="yoursite.multi" required /></label>
+	    </div>
+
+	    <?= $this->draw('forms/domains/dbsettings'); ?>
+	    <?= $this->draw('forms/domains/plugins'); ?>
+
+	    <input type="hidden" name="class" value="ElggMultisite\Domain" />
+	    <input type="submit" name="Submit" value="Submit" />
 	</form>
 </div>
